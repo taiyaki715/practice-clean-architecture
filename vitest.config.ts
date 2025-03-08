4ada0b70c94a10df338890b71d562f7888b9c039
taiki-ishikawa <taiyaki715@icloud.com>
2025-03-08 16:42:13 +0900

    テストカバレッジの出力を追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,11 @@ import tsconfigpaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     plugins: [tsconfigpaths()],
+    test: {
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'json', 'html'],
+            exclude: ['node_modules/', '.git/'],
+        },
+    },
 })
