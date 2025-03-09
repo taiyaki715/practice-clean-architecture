7c063ccdef4337cdf6a580ca32ea34409063a756
taiki-ishikawa <taiyaki715@icloud.com>
2025-03-09 17:30:01 +0900

    dbをDIするように修正

--- src/infra/repositories/todoRepository.ts
+++ src/infra/repositories/todoRepository.ts
@@ -1,18 +1,20 @@
 import { TodoRepositoryInterface } from "@/core/repositories/todoRepositoryInterface";
-import { db } from "../db";
 import { Todo } from "@/core/models/todo";
-import { todos } from "../db/schema";
+import { todos } from "@/infra/db/schema";
 import { UUID } from "@/core/models/uuid";
 import { eq } from "drizzle-orm";
+import { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
 export class TodoRepository implements TodoRepositoryInterface {
+    constructor(private readonly db: PostgresJsDatabase) {}
+
     async getAll(): Promise<Todo[]> {
-        const rawTodos = await db.select().from(todos);
+        const rawTodos = await this.db.select().from(todos);
         return rawTodos.map(rawTodo => new Todo(new UUID(rawTodo.id), rawTodo.title, rawTodo.completed));
     }
 
     async getById(id: string): Promise<Todo | undefined> {
-        const rawTodos = await db.select().from(todos).where(eq(todos.id, id));
+        const rawTodos = await this.db.select().from(todos).where(eq(todos.id, id));
         const todo = rawTodos[0];
         if (!todo) {
             return undefined;
@@ -21,7 +23,7 @@ export class TodoRepository implements TodoRepositoryInterface {
     }
 
     async create(todo: Todo): Promise<Todo> {
-        const rawTodo = await db.insert(todos).values({
+        const rawTodo = await this.db.insert(todos).values({
             id: todo.id.toString(),
             title: todo.title,
             completed: todo.completed
@@ -41,7 +43,7 @@ export class TodoRepository implements TodoRepositoryInterface {
             updateData.completed = updates.completed;
         }
         
-        const rawTodo = await db
+        const rawTodo = await this.db
             .update(todos)
             .set(updateData)
             .where(eq(todos.id, id))
@@ -51,6 +53,6 @@ export class TodoRepository implements TodoRepositoryInterface {
     }
 
     async delete(id: string): Promise<void> {
-        await db.delete(todos).where(eq(todos.id, id));
+        await this.db.delete(todos).where(eq(todos.id, id));
     }
 }
