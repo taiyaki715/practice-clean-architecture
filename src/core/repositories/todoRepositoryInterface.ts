a93259d54ba2f213cac72e8cba0e5a1c72990538
taiki-ishikawa <taiyaki715@icloud.com>
2025-03-09 13:52:30 +0900

    todoRepositoryInterfaceにdocを追加

--- src/core/repositories/todoRepositoryInterface.ts
+++ src/core/repositories/todoRepositoryInterface.ts
@@ -1,9 +1,37 @@
 import { Todo } from "@/core/models/todo";
 
 export interface TodoRepositoryInterface {
+    /**
+     * すべてのTodoを取得する
+     * @returns {Promise<Todo[]>} - すべてのTodo
+     */
     getAll(): Promise<Todo[]>;
+
+    /**
+     * 指定されたIDのTodoを取得する
+     * @param id - TodoのID
+     * @returns {Promise<Todo | undefined>} - 指定されたIDのTodo
+     */
     getById(id: string): Promise<Todo | undefined>;
-    create(todo: Todo): Promise<void>;
-    update(id: string, updates: Partial<Todo>): Promise<void>;
+
+    /**
+     * 新しいTodoを作成する
+     * @param todo - 新しいTodo
+     * @returns {Promise<Todo>} - 作成されたTodo
+     */
+    create(todo: Todo): Promise<Todo>;
+
+    /**
+     * 指定されたIDのTodoを更新する
+     * @param id - TodoのID
+     * @param updates - 更新する内容
+     * @returns {Promise<Todo>} - 更新されたTodo
+     */
+    update(id: string, updates: Partial<Todo>): Promise<Todo>;
+
+    /**
+     * 指定されたIDのTodoを削除する
+     * @param id - TodoのID
+     */
     delete(id: string): Promise<void>;
 }
